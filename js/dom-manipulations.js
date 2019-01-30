a45a80137c125a1733e6447ee98b2363332be341
Ramesh <rpaudel58@gmail.com>
2019-01-30 16:57:17 +0545

    Creating elements and basic manipulations :

--- js/dom-manipulations.js
+++ js/dom-manipulations.js
@@ -27,4 +27,39 @@ container.onmouseleave = function(){
 }
 
 
+// document.body.innerHTML += '<p>Lorem ipsum</p>';
+
+var paragraph = document.createElement('p');
+var paragraph2 = document.createElement('p');
+var  newElement = document.createElement("div");// => <div></div>
+
+
+
+newElement.classList.add('container');
+
+paragraph.innerText = "Some text";
+
+paragraph2.innerText = "Second paragraph"
+
+//Push first paragraph to the div
+newElement.appendChild(paragraph)
+
+//Push second paragrpah to the div
+newElement.appendChild(paragraph2)
+
+setTimeout(()=>{
+    document.body.appendChild(newElement);
+},5000)
+
+
+
+
+
+// User -> ENter the number of times you want to input values
+//  -->8
+
+
+//Enter the number of collegues in your class -> 8
+// Enter their names -> 8 
+// Every name should be in a div with h2 <div><h2>Harry Porter</h2></div>
 
